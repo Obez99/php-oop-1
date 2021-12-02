0e467dc9e05044f7b152d744211bb1f1220ec9e7
Obez99 <giuliogx@live.it>
2021-12-02 14:29:53 +0100

    Adds movies array

---
+++ data/movies.php
@@ -0,0 +1,54 @@
+<?php
+
+$movies = [
+  [
+    "title" => "Venom - La furia di Carnage",
+    "company" => "Marvel",
+    "rating" => 4,
+    "cast" => [
+      "name" => "Tom",
+      "surname" => "Hardy",
+      "age" => 44
+    ],
+    "cast" => [
+      "name" => "Woody",
+      "surname" => "Harrelson",
+      "age" => 60
+    ],
+    "cast" => [
+      "name" => "Michelle",
+      "surname" => "Williams",
+      "age" => 41
+    ],
+    "cast" => [
+      "name" => "Naomie",
+      "surname" => "Harris",
+      "age" => 45
+    ],
+  ],
+  [
+    "title" => "Inception",
+    "company" => "Warner Bros",
+    "rating" => 4,
+    "cast" => [
+      "name" => "Leonardo",
+      "surname" => "Di Caprio",
+      "age" => 47
+    ],
+    "cast" => [
+      "name" => "Joseph",
+      "surname" => "Gordon-Levitt",
+      "age" => 40
+    ],
+    "cast" => [
+      "name" => "Tom",
+      "surname" => "Hardy",
+      "age" => 44
+    ],
+    "cast" => [
+      "name" => "Cillian",
+      "surname" => "Murphy",
+      "age" => 45
+    ],
+  ],
+];
