069baa20527f28b073d31c83b22fe4f001427686
Obez99 <giuliogx@live.it>
2021-12-02 14:35:38 +0100

    Fixes cast array

--- data/movies.php
+++ data/movies.php
@@ -6,49 +6,53 @@ $movies = [
     "company" => "Marvel",
     "rating" => 4,
     "cast" => [
-      "name" => "Tom",
-      "surname" => "Hardy",
-      "age" => 44
-    ],
-    [
-      "name" => "Woody",
-      "surname" => "Harrelson",
-      "age" => 60
-    ],
-    [
-      "name" => "Michelle",
-      "surname" => "Williams",
-      "age" => 41
-    ],
-    [
-      "name" => "Naomie",
-      "surname" => "Harris",
-      "age" => 45
-    ],
+      [
+        "name" => "Tom",
+        "surname" => "Hardy",
+        "age" => 44
+      ],
+      [
+        "name" => "Woody",
+        "surname" => "Harrelson",
+        "age" => 60
+      ],
+      [
+        "name" => "Michelle",
+        "surname" => "Williams",
+        "age" => 41
+      ],
+      [
+        "name" => "Naomie",
+        "surname" => "Harris",
+        "age" => 45
+      ]
+    ]
   ],
   [
     "title" => "Inception",
     "company" => "Warner Bros",
     "rating" => 4,
     "cast" => [
-      "name" => "Leonardo",
-      "surname" => "Di Caprio",
-      "age" => 47
-    ],
-    [
-      "name" => "Joseph",
-      "surname" => "Gordon-Levitt",
-      "age" => 40
-    ],
-    [
-      "name" => "Tom",
-      "surname" => "Hardy",
-      "age" => 44
-    ],
-    [
-      "name" => "Cillian",
-      "surname" => "Murphy",
-      "age" => 45
-    ],
-  ],
+      [
+        "name" => "Leonardo",
+        "surname" => "Di Caprio",
+        "age" => 47
+      ],
+      [
+        "name" => "Joseph",
+        "surname" => "Gordon-Levitt",
+        "age" => 40
+      ],
+      [
+        "name" => "Tom",
+        "surname" => "Hardy",
+        "age" => 44
+      ],
+      [
+        "name" => "Cillian",
+        "surname" => "Murphy",
+        "age" => 45
+      ],
+    ]
+  ]
 ];
