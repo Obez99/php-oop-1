ffab485b20f33ea0d17be2f9ef99a1639c404be0
Obez99 <giuliogx@live.it>
2021-12-05 18:02:21 +0100

    Adds setter functions to set movie details

--- classes/Movie.php
+++ classes/Movie.php
@@ -11,25 +11,10 @@ class Movie
 
   function __construct($_movieData)
   {
-    if (!array_key_exists("title", $_movieData)) {
-      $this->movieData["title"] = "Nessun titolo.";
-    } else
-      $this->movieData["title"] = $_movieData["title"];
-
-    if (!array_key_exists("company", $_movieData)) {
-      $this->movieData["company"] = "Nessuna casa produttrice.";
-    } else
-      $this->movieData["company"] = $_movieData["company"];
-
-    if (!array_key_exists("rating", $_movieData)) {
-      $this->movieData["rating"] = "Nessun voto.";
-    } else
-      $this->movieData["rating"] = $_movieData["rating"];
-
-    if (!array_key_exists("cast", $_movieData)) {
-      $this->movieData["cast"] = "Nessun cast.";
-    } else
-      $this->movieData["cast"] = $_movieData["cast"];
+    $this->setMovieTitle($_movieData["title"]);
+    $this->setMovieCompany($_movieData["company"]);
+    $this->setMovieRating($_movieData["rating"]);
+    $this->setMovieCast($_movieData["cast"]);
   }
 
   public function getMovieTitle()
@@ -47,5 +32,22 @@ class Movie
   public function getMovieCast()
   {
     return $this->movieData["cast"];
+  }
+
+  public function setMovieTitle($title)
+  {
+    $this->movieData["title"] = $title;
+  }
+  public function setMovieCompany($company)
+  {
+    $this->movieData["company"] = $company;
+  }
+  public function setMovieRating($rating)
+  {
+    $this->movieData["rating"] = $rating;
+  }
+  public function setMovieCast($cast)
+  {
+    $this->movieData["cast"] = $cast;
   }
 }
